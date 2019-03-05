9444d5c636d8ab2798308a7d83187b73a977ca9a
moguera57 <josephbanwart@gmail.com>
2019-03-05 18:46:04 -0500

    seeds oops

---
+++ seeds.sql
@@ -0,0 +1,7 @@
+USE bamazon_db;
+
+INSERT INTO products (item_name,category,price,stock_quantity) VALUES ('Shoes', 'Shoes', 200, 200);
+INSERT INTO products (item_name,category,price,stock_quantity) VALUES ('Miniature', 'Toys', 100, 20);
+INSERT INTO products (item_name,category,price,stock_quantity) VALUES ('THing', 'Stuff', 100, 5);
+INSERT INTO products (item_name,category,price,stock_quantity) VALUES ('Other THing', 'Stuff', 10, 20);
+INSERT INTO products (item_name,category,price,stock_quantity) VALUES ('Last THing', 'Toys', 10, 1);
